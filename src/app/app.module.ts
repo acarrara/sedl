e6a669fee00b94bc811c4f8f13a1285f8866917f
Alessandro Carrara <alessandro.carrara@gmail.com>
2019-06-12 13:46:02 +0200

    add hammerjs configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {BrowserModule} from '@angular/platform-browser';
+import {BrowserModule, HAMMER_GESTURE_CONFIG, HammerGestureConfig} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {CellComponent} from './cell.component';
@@ -9,6 +9,14 @@ import {LordRegionElementComponent} from './header/lord-region-element.component
 import {GameService} from './game.service';
 import {CountPipe} from './header/seCount.pipe';
 import {HeaderComponent} from './header/header.component';
+
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = {
+    swipe: {direction: Hammer.DIRECTION_ALL}, // override default settings
+    pan: {direction: Hammer.DIRECTION_ALL},
+    pinch: {enable: true, direction: Hammer.DIRECTION_ALL}
+  } as any;
+}
 
 @NgModule({
   declarations: [
@@ -24,7 +32,10 @@ import {HeaderComponent} from './header/header.component';
   imports: [
     BrowserModule
   ],
-  providers: [GameService],
+  providers: [GameService, {
+    provide: HAMMER_GESTURE_CONFIG,
+    useClass: MyHammerConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
