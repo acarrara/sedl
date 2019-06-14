e57441370cac5149c8a6a7c6761ce6e3b63daf46
Alessandro Carrara <alessandro.carrara@gmail.com>
2019-06-14 14:15:27 +0200

    better message in region statistics

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -30,8 +30,10 @@ export class FooterComponent {
         return 'Hill';
       case 'f':
         return 'Forest';
+      case 'm':
+        return 'Mountain';
       default:
-        return 'Unkwnown';
+        return 'Unknown';
     }
   }
 
