95f22fce8ee0126cea0e4630961a10c95dfe1e4f
Alessandro Carrara <alessandro.carrara@gmail.com>
2019-06-17 18:36:16 +0200

    support pan when zoomed

--- src/app/pinch-and-zoom.directive.ts
+++ src/app/pinch-and-zoom.directive.ts
@@ -7,6 +7,8 @@ export class PinchAndZoomDirective {
 
   private lastScale = 1;
   private scale = 1;
+  private posX = 0;
+  private posY = 0;
   private lastPosX = 0;
   private lastPosY = 0;
   private maxPosY = 0;
@@ -19,25 +21,40 @@ export class PinchAndZoomDirective {
   public onPinch($event: any) {
     const scale = Math.max(.999, Math.min(this.lastScale * ($event.scale), 4));
     this.scale = scale;
-    if (scale !== 1) {
-      let posX = this.lastPosX + $event.deltaX;
-      let posY = this.lastPosY + $event.deltaY;
-      this.maxPosX = Math.ceil((scale - 1) * this.element.nativeElement.clientWidth / 2);
-      this.maxPosY = Math.ceil((scale - 1) * this.element.nativeElement.clientHeight / 2);
-      if (posX > this.maxPosX) {
-        posX = this.maxPosX;
+    this.transform($event);
+  }
+
+  @HostListener('pan', ['$event'])
+  public onPan($event: any) {
+    this.transform($event);
+  }
+
+  @HostListener('panend')
+  public onPanEnd() {
+    this.lastPosX = this.posX < this.maxPosX ? this.posX : this.maxPosX;
+    this.lastPosY = this.posY < this.maxPosY ? this.posY : this.maxPosY;
+  }
+
+  private transform($event: any) {
+    if (this.scale !== 1) {
+      this.posX = this.lastPosX + $event.deltaX;
+      this.posY = this.lastPosY + $event.deltaY;
+      this.maxPosX = Math.ceil((this.scale - 1) * this.element.nativeElement.clientWidth / 2);
+      this.maxPosY = Math.ceil((this.scale - 1) * this.element.nativeElement.clientHeight / 2);
+      if (this.posX > this.maxPosX) {
+        this.posX = this.maxPosX;
       }
-      if (posX < -this.maxPosX) {
-        posX = -this.maxPosX;
+      if (this.posX < -this.maxPosX) {
+        this.posX = -this.maxPosX;
       }
-      if (posY > this.maxPosY) {
-        posY = this.maxPosY;
+      if (this.posY > this.maxPosY) {
+        this.posY = this.maxPosY;
       }
-      if (posY < -this.maxPosY) {
-        posY = -this.maxPosY;
+      if (this.posY < -this.maxPosY) {
+        this.posY = -this.maxPosY;
       }
       this.element.nativeElement.style.transform =
-        'scale3d(' + scale + ', ' + scale + ', 1) translate3d(' + posX + 'px,' + posY + 'px, 0)';
+        'scale3d(' + this.scale + ', ' + this.scale + ', 1) translate3d(' + this.posX + 'px,' + this.posY + 'px, 0)';
     }
   }
 
