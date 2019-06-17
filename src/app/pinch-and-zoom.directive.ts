43eb1d31c917f5261139c591a45db16abc0abdb6
Alessandro Carrara <alessandro.carrara@gmail.com>
2019-06-17 08:09:16 +0200

    pan on pinch and zoom

--- src/app/pinch-and-zoom.directive.ts
+++ src/app/pinch-and-zoom.directive.ts
@@ -7,7 +7,10 @@ export class PinchAndZoomDirective {
 
   private lastScale = 1;
   private scale: number;
-  private doubled;
+  private lastPosX: number;
+  private lastPosY: number;
+  private maxPosY: number;
+  private maxPosX: number;
 
   constructor(private element: ElementRef) {
   }
@@ -17,27 +20,30 @@ export class PinchAndZoomDirective {
     const scale = Math.max(.999, Math.min(this.lastScale * ($event.scale), 4));
     this.scale = scale;
     if (scale !== 1) {
-      this.element.nativeElement.style.transform = 'scale3d(' + scale + ', ' + scale + ', 1)';
+      let posX = this.lastPosX + $event.deltaX;
+      let posY = this.lastPosY + $event.deltaY;
+      this.maxPosX = Math.ceil((scale - 1) * this.element.nativeElement.clientWidth / 2);
+      this.maxPosY = Math.ceil((scale - 1) * this.element.nativeElement.clientHeight / 2);
+      if (posX > this.maxPosX) {
+        posX = this.maxPosX;
+      }
+      if (posX < -this.maxPosX) {
+        posX = -this.maxPosX;
+      }
+      if (posY > this.maxPosY) {
+        posY = this.maxPosY;
+      }
+      if (posY < -this.maxPosY) {
+        posY = -this.maxPosY;
+      }
+      this.element.nativeElement.style.transform =
+        'scale3d(' + scale + ', ' + scale + ', 1) translate3d(' + posX + 'px,' + posY + 'px, 0) ';
     }
   }
 
   @HostListener('pinchend')
   public onPinchEnd() {
     this.lastScale = this.scale;
-  }
-
-  @HostListener('dblclick')
-  public onDoubleClick() {
-    const scale = this.doubled ? 1 : 2;
-    this.doubled = !this.doubled;
-    this.element.nativeElement.style.transform = 'scale3d(' + scale + ', ' + scale + ', 1)';
-  }
-
-  @HostListener('doubletap')
-  public onDoubleTap() {
-    const scale = this.doubled ? 1 : 2;
-    this.doubled = true;
-    this.element.nativeElement.style.transform = 'scale3d(' + scale + ', ' + scale + ', 1)';
   }
 
 }
