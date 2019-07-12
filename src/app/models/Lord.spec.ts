3b45a55bbb3dcf197697bf18d663f5250d802f4a
Alessandro Carrara <alessandro.carrara@gmail.com>
2019-07-12 23:57:06 +0200

    more lord tests

--- src/app/models/Lord.spec.ts
+++ src/app/models/Lord.spec.ts
@@ -40,4 +40,62 @@ describe('Lord', () => {
       expect(aLord.canPlay()).toBeFalsy();
     });
   });
+
+  describe('canRush', () => {
+
+    it('should return true when lord has not rushed', () => {
+      const aLord = new Lord('l1');
+
+      expect(aLord.canRush()).toBeTruthy();
+    });
+
+    it('should return false when lord has rushed', () => {
+      const aLord = new Lord('l1');
+      aLord.board = new Board(['s'], ['u']);
+      aLord.rush();
+
+      expect(aLord.canRush()).toBeFalsy();
+    });
+  });
+
+  describe('rush', () => {
+
+    it('should add 20% of worth to the treasure', () => {
+      const aLord = new Lord('l1');
+      aLord.treasure = 10;
+      aLord.board = new Board(['s', 'm', 'm', 'm'], ['l1', 'l1', 'l1', 'l1']);
+
+      aLord.rush();
+
+      expect(aLord.treasure).toEqual(11);
+    });
+  });
+
+  describe('canSettle', () => {
+
+    it('should return true when Lord has still available settlements', () => {
+      const aLord = new Lord('l1');
+
+      expect(aLord.canSettle()).toBeTruthy();
+    });
+
+    it('should return false when Lord has no available settlements', () => {
+      const aLord = new Lord('l1');
+      aLord.availableSettlements = 0;
+
+      expect(aLord.canSettle()).toBeFalsy();
+    });
+  });
+
+  describe('settle', () => {
+
+    it('should settle the region', () => {
+      const aLord = new Lord('l1');
+      aLord.board = new Board(['s', 'm', 'm', 'm'], ['l1', 'l1', 'l1', 'l1']);
+
+      aLord.settle(aLord.board.regions[1]);
+
+      expect(aLord.board.regions[1].isSettlement()).toBeTruthy();
+    });
+  });
 });
